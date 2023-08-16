84d54b67f10db95acecdb80a2d105c19fd0a87de
mariumzee <90246281+mariumzee@users.noreply.github.com>
2023-08-16 16:50:35 +0200

    Add files via upload

---
+++ JavaEE/src/main/java/de/uni_koblenz/javaee/greeting/HelloBean.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package de.uni_koblenz.javaee.greeting;
+   import javax.enterprise.context.RequestScoped;
+        import javax.inject.Named;
+
+/**
+ *
+ * @author zomzulfi
+ */
+public class HelloBean {
+       public String getMessage() {
+                return "Hello, Course!";
+            }
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
